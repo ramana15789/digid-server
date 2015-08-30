cd95c16ba3cbd03072ca37fe2d5e2dbdb57857fb
durga.p <durga.p@flipkart.com>
2015-08-30 11:44:04 +0530

    fixes

--- public/js/report.js
+++ public/js/report.js
@@ -41,9 +41,13 @@ function renderRows(firstTime) {
                if (fields.length > window.lastLoadNum) {
                    $("#alert").show().delay(2000).fadeOut()
                }
+               else {
+                   return;
+               }
             }
             window.lastLoadNum = fields.length;
 
+            $("#report_rows").empty();
             for (var x=0; x < fields.length; x++) {
                 $("#report_rows").append(templates.row_field(fields[x]))
             }
